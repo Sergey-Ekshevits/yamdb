ddaeccf2f10c45e634a078cbbbe5bbd9ff2e948c
Sergey <sekshevits@gmail.com>
2023-08-08 20:35:23 +0400

    добавил required fields email в модель CustomUser

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -15,4 +15,4 @@ class CustomUser(AbstractUser):
                             default='user',
                             blank=False,
                             max_length=25)
-    REQUIRED_FIELDS = ['username', 'email']
+    REQUIRED_FIELDS = ['email']
