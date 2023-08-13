db7a4d1a1d3c032b6e328ecb1a9fe88c6eb9e3c2
Sergey <sekshevits@gmail.com>
2023-08-13 15:28:45 +0400

    добавил миксины в сериализаторы

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -8,44 +8,7 @@ from users.models import ROLES
 User = get_user_model()
 
 
-class RegistrationSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=150)
-    email = serializers.EmailField(max_length=254)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email']
-
-
-    def validate_username(self, value):
-        if value.lower() == 'me' or not re.match("^[\w.@+-]+$", value):
-            raise serializers.ValidationError('Некорректное имя пользователя')
-        return value
-
-
-class ConfirmationCodeSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=150)
-    confirmation_code = serializers.CharField()
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(max_length=150, required=True)
-    email = serializers.EmailField(max_length=254)
-    first_name = serializers.CharField(max_length=150, required=False)
-    last_name = serializers.CharField(max_length=150, required=False)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio', 'role')
-        read_only_fields = ['role']
-
-    def validate_username(self, value):
-        if value.lower() == 'me' or not re.match("^[\w.@+-]+$", value):
-            raise serializers.ValidationError('Некорректное имя пользователя')
-        return value
-
-
-class UsersSerializer(serializers.ModelSerializer):
+class UserSerializerMixin(serializers.Serializer):
     username = serializers.CharField(max_length=150,
                                      required=True,
                                      validators=[UniqueValidator(queryset=User.objects.all())])
@@ -53,7 +16,11 @@ class UsersSerializer(serializers.ModelSerializer):
                                    validators=[UniqueValidator(queryset=User.objects.all())])
     first_name = serializers.CharField(max_length=150, required=False)
     last_name = serializers.CharField(max_length=150, required=False)
-    role = serializers.ChoiceField(required=False, default='user', choices=ROLES)
+
+    def validate_username(self, value):
+        if value.lower() == 'me' or not re.match("^[\w.@+-]+$", value):
+            raise serializers.ValidationError('Некорректное имя пользователя')
+        return value
 
     class Meta:
         model = User
@@ -66,12 +33,24 @@ class UsersSerializer(serializers.ModelSerializer):
             ),
         ]
 
-    def validate_username(self, value):
-        if value.lower() == 'me' or not re.match("^[\w.@+-]+$", value):
-            raise serializers.ValidationError('Некорректное имя пользователя')
-        return value
 
-    def validate_role(self, value):
-        if value not in ('admin', 'user', 'moderator'):
-            raise serializers.ValidationError('Некорректная роль пользователя')
-        return value
+class RegistrationSerializer(UserSerializerMixin, serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ConfirmationCodeSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    confirmation_code = serializers.CharField()
+
+
+class UserProfileSerializer(UserSerializerMixin, serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'bio', 'role')
+        read_only_fields = ['role']
+
+
+class UsersSerializer(UserSerializerMixin, serializers.ModelSerializer):
+    role = serializers.ChoiceField(required=False, default='user', choices=ROLES)
