e7b657ff9b88c693d9f8963ba4e212e783d802c4
Sergey <sekshevits@gmail.com>
2023-08-16 20:07:39 +0400

    Поправил импорты 3

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -2,9 +2,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
-
 from rest_framework import filters, status, viewsets
-from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
